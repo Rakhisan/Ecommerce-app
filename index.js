dbb12f6ef4e375a90de4308bf7df5714da5086cd
Rakhi Singh <rakhisingh030118@gmail.com>
2024-09-11 12:22:46 +0530

    updates

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import mongoose from "mongoose"; //
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
@@ -15,6 +16,8 @@ const __dirname = path.dirname(__filename);
 
 //configure env
 dotenv.config();
+
+mongoose.set("strictQuery", false); // Add this line to configure strictQuery
 
 //databse config
 connectDB();
@@ -34,8 +37,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-
+// Serve static files
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
