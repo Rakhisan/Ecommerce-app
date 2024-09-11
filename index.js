c74f53a27450073cdf8192d3d6325563c001ff8e
Rakhi Singh <rakhisingh030118@gmail.com>
2024-09-11 16:52:03 +0530

    updates

--- index.js
+++ index.js
@@ -4,60 +4,57 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import mongoose from "mongoose"; //
+import mongoose from "mongoose";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Configure environment variables
+dotenv.config();
+
+mongoose.set("strictQuery", false); // Configure strictQuery for Mongoose
+
+// Connect to the database
+connectDB();
+
+// Get current filename and directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//configure env
-dotenv.config();
-
-mongoose.set("strictQuery", false); // Add this line to configure strictQuery
-
-//databse config
-connectDB();
-
-//rest object
+// Initialize Express app
 const app = express();
 
-//middelwares
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Serve static files from the client build folder
 app.use(express.static(path.join(__dirname, "./client/build")));
-//
-//routes
+
+// API routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// Serve index.html for all non-API routes
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"), (err) => {
-//     if (err) {
-//       res.status(404).send("404 Not Found");
-//     }
-//   });
-// });
+// Catch-all route to serve React's index.html for any non-API routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"), (err) => {
+    if (err) {
+      res.status(404).send("404 Not Found");
+    }
+  });
+});
 
-//PORT
+// Set the port
 const PORT = process.env.PORT || 8080;
 
-// app.js
-import { getConfig } from "./config.js";
-
-console.log(getConfig()); // Access process.env.CONFIG_VARIABLE dynamically
-
-//run listen
-app.listen(8080, () => {
+// Start the server
+app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${8080}`.bgCyan
+    `Server Running in ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .white
   );
 });
