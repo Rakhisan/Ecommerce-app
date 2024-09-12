456cda19bd968cb5e1c023fb994caa7355894277
Rakhi Singh <rakhisingh030118@gmail.com>
2024-09-12 10:40:32 +0530

    updates

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -35,9 +35,10 @@ const ProductDetails = () => {
     }
   }, []);
 
-  // Initial product details load
   useEffect(() => {
-    if (params?.slug) getProduct();
+    if (params?.slug) {
+      getProduct();
+    }
   }, [params?.slug, getProduct]);
 
   return (
@@ -55,16 +56,16 @@ const ProductDetails = () => {
         <div className="col-md-6 product-details-info">
           <h1 className="text-center">Product Details</h1>
           <hr />
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
+          <h6>Name: {product.name}</h6>
+          <h6>Description: {product.description}</h6>
           <h6>
-            Price :
+            Price:{" "}
             {product?.price?.toLocaleString("en-US", {
               style: "currency",
               currency: "USD",
             })}
           </h6>
-          <h6>Category : {product?.category?.name}</h6>
+          <h6>Category: {product?.category?.name}</h6>
           <button className="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
       </div>
@@ -92,9 +93,7 @@ const ProductDetails = () => {
                     })}
                   </h5>
                 </div>
-                <p className="card-text ">
-                  {p.description.substring(0, 60)}...
-                </p>
+                <p className="card-text">{p.description.substring(0, 60)}...</p>
                 <div className="card-name-price">
                   <button
                     className="btn btn-info ms-1"
@@ -102,19 +101,6 @@ const ProductDetails = () => {
                   >
                     More Details
                   </button>
-                  {/* <button
-                  className="btn btn-dark ms-1"
-                  onClick={() => {
-                    setCart([...cart, p]);
-                    localStorage.setItem(
-                      "cart",
-                      JSON.stringify([...cart, p])
-                    );
-                    toast.success("Item Added to cart");
-                  }}
-                >
-                  ADD TO CART
-                </button> */}
                 </div>
               </div>
             </div>
